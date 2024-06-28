a73f0fe0167ff835ea052fb253ed0be2b40e6053
HUBERTBRICAU <hubertbricau@gmail.com>
2024-06-28 06:15:39 +0200

    2906

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function myFunction() {
   document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
   document.getElementById("logo").innerHTML = "Het Courtys-chalet";
   document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
+  document.getElementById("clickSurPhoto").innerHTML = "Klik op de foto voor kaarten";
   document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
   document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
@@ -16,6 +17,7 @@ function Français() {
   document.getElementById("h1id").innerHTML = "Le chalet Courtys";
   document.getElementById("logo").innerHTML = "Le chalet Courtys";
   document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
+  document.getElementById("clickSurPhoto").innerHTML = "Cliquer sur la photo pour maps";
   document.getElementById("Bayart").innerHTML = "Les 4 fils Aymon, le cheval Bayart";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, le tunnel du canal";
   document.getElementById("dames").innerHTML = "Laifour, les dames de Meuse";
