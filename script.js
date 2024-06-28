931b411057ea4ddca4dffca2be87d8997236c017
HUBERTBRICAU <hubertbricau@gmail.com>
2024-06-28 05:46:31 +0200

    neerb

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+
+  document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
+  document.getElementById("logo").innerHTML = "Het Courtys-chalet";
+  document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
+  document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
+  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
+  document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
+  document.getElementById("eglise").innerHTML = "Saint-Aignan, de kerk";
+  document.getElementById("eglise").innerHTML = "Saint-Aignan, de kerk";
+  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, het hertogelijk plein";
+  document.getElementById("chateauSedan").innerHTML = "Kasteel Sedan";
+
+}
+function Français() {
+  document.getElementById("h1id").innerHTML = "Le chalet Courtys";
+  document.getElementById("logo").innerHTML = "Le chalet Courtys";
+  document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
+  document.getElementById("Bayart").innerHTML = "Les 4 fils Aymon, le cheval Bayart";
+  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, le tunnel du canal";
+  document.getElementById("dames").innerHTML = "Laifour, les dames de Meuse";
+  document.getElementById("eglise").innerHTML = "Saint-Aignan, l'église";
+  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, la place ducale";
+  document.getElementById("chateauSedan").innerHTML = "Le château de Sedan";
+}
