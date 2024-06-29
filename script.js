8521fa9305489ee567c0b9521d102cdc94e63a8a
HUBERTBRICAU <hubertbricau@gmail.com>
2024-06-29 09:24:37 +0200

    2906

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function Neerlandais() {
   document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
   document.getElementById("logo").innerHTML = "Het Courtys-chalet";
   document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
-  // document.getElementById("clickSurPhoto").innerHTML = "Klik op de foto voor kaarten";
+  document.getElementById("textNaturecmlic").innerHTML = "Klik om naar Kaarten te gaan";
+  document.getElementById("Neerlandais").innerHTML = "Frans";
+  
+  
   document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
   document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
@@ -16,8 +19,10 @@ function Neerlandais() {
 function Français() {
   document.getElementById("h1id").innerHTML = "Le chalet Courtys";
   document.getElementById("logo").innerHTML = "Le chalet Courtys";
+  document.getElementById("Frans").innerHTML = "Français";
+  document.getElementById("Neerlandais").style.visibility = "hidden";
   document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
-  // document.getElementById("clickSurPhoto").innerHTML = "Cliquer sur la photo pour maps";
+  document.getElementById("textNaturecmlic").innerHTML = "Cliquer pour aller sur Maps";
   document.getElementById("Bayart").innerHTML = "Les 4 fils Aymon, le cheval Bayart";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, le tunnel du canal";
   document.getElementById("dames").innerHTML = "Laifour, les dames de Meuse";
