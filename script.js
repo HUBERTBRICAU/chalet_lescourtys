b40c1143969126ab1cd90e376612ddff1ee161c3
HUBERTBRICAU <hubertbricau@gmail.com>
2024-06-30 12:28:48 +0200

    france

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let langue = "français";
+
+
+  const langueFrancais = "Français";
+  const langueNeerl = "Neerlandais";
 
 function Neerlandais() {
   document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
   document.getElementById("logo").innerHTML = "Het Courtys-chalet";
   document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
   document.getElementById("textNaturecmlic").innerHTML = "Klik om naar Kaarten te gaan";
-  
-  // document.getElementById("Neerlandais").innerHTML = langue;
-  
+  document.getElementById("Neerlandais").innerHTML = langueFrancais;
   document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
   document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
@@ -21,7 +22,7 @@ function Neerlandais() {
 function Français() {
   document.getElementById("h1id").innerHTML = "Le chalet Courtys";
   document.getElementById("logo").innerHTML = "Le chalet Courtys";
-  document.getElementById("Frans").innerHTML = langue;
+  document.getElementById("Français").innerHTML = langueNeerl;
   // document.getElementById("Neerlandais").style.visibility = "hidden";
   document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
   document.getElementById("textNaturecmlic").innerHTML = "Cliquer pour aller sur Maps";
