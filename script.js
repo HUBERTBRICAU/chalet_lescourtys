798400e787ccb66966717f09f68b7f539f10e596
HUBERTBRICAU <hubertbricau@gmail.com>
2024-07-01 09:25:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Neerlandais() {
   document.getElementById("logo").innerHTML = "Het Courtys-chalet";
   document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
   document.getElementById("textNaturecmlic").innerHTML = "Klik om naar Kaarten te gaan";
-  document.getElementById("Neerlandais").innerHTML = langueFrancais;
+  // document.getElementById("Neerlandais").innerHTML = langueFrancais;
   document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
   document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
   document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
@@ -22,7 +22,7 @@ function Neerlandais() {
 function Français() {
   document.getElementById("h1id").innerHTML = "Le chalet Courtys";
   document.getElementById("logo").innerHTML = "Le chalet Courtys";
-  document.getElementById("Français").innerHTML = langueNeerl;
+  // document.getElementById("Français").innerHTML = langueNeerl;
   // document.getElementById("Neerlandais").style.visibility = "hidden";
   document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
   document.getElementById("textNaturecmlic").innerHTML = "Cliquer pour aller sur Maps";
