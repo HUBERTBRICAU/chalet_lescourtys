6139ae649c69e77ad196f18a12ee659b17a82b3f
HUBERTBRICAU <hubertbricau@gmail.com>
2024-07-13 20:50:16 +0200

    15

--- script.js
+++ script.js
@@ -2,37 +2,12 @@
 
   const langueFrancais = "Français";
   const langueNeerl = "Nederlands";
-function Neerlandais() {
-  document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
-  document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
-  document.getElementById("textNaturecmlic").innerHTML = "Klik om naar Kaarten te gaan";
-  document.getElementById("lechalet").innerHTML = "Het huisje";
-  document.getElementById("nosforets").innerHTML = "Onze bossen";
-  document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
-  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
-  document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
-  document.getElementById("eglise").innerHTML = "Saint-Aignan, de kerk";
-  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, hertogelijk plein";
-  document.getElementById("chateauSedan").innerHTML = "Kasteel Sedan";
+// function Neerlandais() {
 
-  document.getElementById("riviere").innerHTML = "Een rivier";
-  document.getElementById("Champignons").innerHTML = "Paddestoelen";
-}
-function Français() {
-  document.getElementById("h1id").innerHTML = "Le chalet Courtys";
-  document.getElementById("nosforets").innerHTML = "Nos forêts";
-  document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
-  document.getElementById("textNaturecmlic").innerHTML = "Cliquer pour aller sur Maps";
-  document.getElementById("Bayart").innerHTML = "Les 4 fils Aymon, le cheval Bayart";
-  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, le tunnel du canal";
-  document.getElementById("dames").innerHTML = "Laifour, les dames de Meuse";
-  document.getElementById("eglise").innerHTML = "Saint-Aignan, l'église";
-  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, place ducale";
-  document.getElementById("chateauSedan").innerHTML = "Le château de Sedan";
+// }
+// function Français() {
 
-  document.getElementById("riviere").innerHTML = "Une riviére";
-  document.getElementById("Champignons").innerHTML = "Champignons";
-}
+// }
 
 let paragraphe= document.getElementById("paragraphe");
 let image =document.getElementById("image");
@@ -52,6 +27,19 @@ function maLangue() {
   document.getElementById("image").style.width="50px"; 
   document.getElementById("leVillage").innerHTML = "Het dorp";
   document.getElementById("leChalet").innerHTML = "Het huisje";
+  document.getElementById("h1id").innerHTML = "Het Courtys-chalet";
+  document.getElementById("remarquables").innerHTML = "Een idee van enkele opmerkelijke plekken in de Ardennen";
+  document.getElementById("textNaturecmlic").innerHTML = "Klik om naar Kaarten te gaan";
+  document.getElementById("lechalet").innerHTML = "Het huisje";
+  document.getElementById("nosforets").innerHTML = "Onze bossen";
+  document.getElementById("Bayart").innerHTML = "De 4 zonen Aymon, het Bayart-paard";
+  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, de kanaaltunnel";
+  document.getElementById("dames").innerHTML = "Laifour, de dames van Maas";
+  document.getElementById("eglise").innerHTML = "Saint-Aignan, de kerk";
+  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, hertogelijk plein";
+  document.getElementById("chateauSedan").innerHTML = "Kasteel Sedan";
+  document.getElementById("riviere").innerHTML = "Een rivier";
+  document.getElementById("Champignons").innerHTML = "Paddestoelen";
   }
   else{
   etat=false;
@@ -61,6 +49,18 @@ function maLangue() {
   srcImage="images/esp32.png" ;
   document.getElementById("leVillage").innerHTML = "Le village";
   document.getElementById("leChalet").innerHTML = "Le chalet";
+  document.getElementById("h1id").innerHTML = "Le chalet Courtys";
+  document.getElementById("nosforets").innerHTML = "Nos forêts";
+  document.getElementById("remarquables").innerHTML = "Une idée de quelques sites remarquables des Ardennes";
+  document.getElementById("textNaturecmlic").innerHTML = "Cliquer pour aller sur Maps";
+  document.getElementById("Bayart").innerHTML = "Les 4 fils Aymon, le cheval Bayart";
+  document.getElementById("tunnelCanal").innerHTML = "Saint-Aignan, le tunnel du canal";
+  document.getElementById("dames").innerHTML = "Laifour, les dames de Meuse";
+  document.getElementById("eglise").innerHTML = "Saint-Aignan, l'église";
+  document.getElementById("placeDucale").innerHTML = "Charleville-Mézières, place ducale";
+  document.getElementById("chateauSedan").innerHTML = "Le château de Sedan";
+  document.getElementById("riviere").innerHTML = "Une riviére";
+  document.getElementById("Champignons").innerHTML = "Champignons";
   }
 }
 
